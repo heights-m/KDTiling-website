6161a4ac838df1393c49aa9b43c973905f5597fb
heights-m <ryupii.24@gmail.com>
2025-03-12 23:10:38 +0900

    add individual project page

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path='/projects' element={<Projects/>}/>
+        <Route path='/projects/project' element={<Projects/>}/>
       </Routes>
       <Footer/>
     </>
